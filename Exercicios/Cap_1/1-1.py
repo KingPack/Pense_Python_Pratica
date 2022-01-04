624c9f6cad6333ee86226c70c7dfb43001f9daef
Hendrek Rodrigues <llhendrek@gmail.com>
2022-01-04 13:17:08 -0300

    update

---
+++ Exercicios/Cap_1/1-1.py
@@ -0,0 +1,28 @@
+# 1. Em uma instrução print, o que acontece se você omitir um dos parênteses ou ambos?
+
+print('teste'    # Erro de syntax
+
+print'test'        # Erro de syntax tambem
+
+
+# 2. Se estiver tentando imprimir uma string, o que acontece se omitir uma das aspas ou ambas?
+
+print(test)  # Valor ou variavel nao definida
+
+print('test) # erro de sintax
+
+
+# 3. Você pode usar um sinal de menos para fazer um número negativo como -2. O que acontece se puser um sinal de mais antes de um número? E se escrever assim: 2++2?
+
+print(2++2) # Soma 2 + 2 como se o ultimo numero fosse positivo ficando (2) + (+2)
+
+
+# 4. Na notação matemática, zeros à esquerda são aceitáveis, como em 02. O que acontece se você tentar usar isso no Python?
+
+print(02+2)  # erro de syntax
+
+
+# 5. O que acontece se você tiver dois valores sem nenhum operador entre eles?
+
+print(22) # imprimi os numeros. mesmo no terminal esse seria o resultado padrao
+
---
+++ Exercicios/Cap_1/1-2.py
@@ -0,0 +1,21 @@
+# 1. Quantos segundos há em 42 minutos e 42 segundos?
+
+minutos = 42
+segundos = 42
+
+calculo_segundos = (minutos * 60) + segundos
+
+print(f'Segundos: {calculo_segundos}s')
+
+
+# 2. Quantas milhas há em 10 quilômetros? Dica: uma milha equivale a 1,61 quilômetro.
+
+quilometros = 10
+
+calculo_milhas = quilometros * 1.61
+
+print(f'{quilometros} quilometros e igual a {calculo_milhas} milhas')
+
+
+# 3. Se você correr 10 quilômetros em 42 minutos e 42 segundos, qual é o seu passo médio (tempo por milha em minutos e segundos)? Qual é a sua velocidade média em milhas por hora?
+
